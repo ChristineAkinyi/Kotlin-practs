541a5f279e84f745d28271da2dafe9fdfa9706b4
ChristineAkinyi <christinneakinyi@gmail.com>
2024-03-19 22:13:56 +0300

    Add new project on functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,48 @@
+fun main() {
+    addNumbers(24,34)
+    addArrayIntegers(arrayOf(2,5,7,9))
+    checkLen(arrayOf("Kenya", "Rwanda", "Uganda", "Ethiopia", "Malawi"))
+    countVowels("Otieno")
+    reverseStr("Lunch")
+
+}
+
+fun addNumbers(num1:Int, num2:Int): Int{
+    var result = num1 + num2
+    return result
+}
+
+fun addArrayIntegers(nums:Array<Int>):Int{
+    var nums = arrayOf(2,5,7,9)
+    var sum = nums[0] + nums[1] +nums[2] + nums[3]
+    return sum
+}
+
+fun checkLen(text:Array<String>):Int{
+    var text = arrayOf("Kenya", "Rwanda", "Uganda", "Ethiopia", "Malawi")
+    var maxLength = 0
+    for (x in text) {
+        if (x.length > maxLength) {
+            maxLength = x.length
+        }
+    }
+    return maxLength
+}
+
+fun countVowels(word:String):Int{
+var vowels = listOf<Char>('a', 'e', 'i', 'o', 'u')
+    var vowelCount = 0
+    for (v in word){
+        if(v in vowels){
+            vowelCount++
+        }
+    }
+    return vowelCount
+}
+
+fun reverseStr(input:String):String{
+    var newStr = input.reversed()
+    return newStr
+
+}
+
